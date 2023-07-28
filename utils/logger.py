eb24ea5e3e45dbdc6b9632ce3d93c390fb4c1feb
DimuthuLakmal <kjtdimuthu.13@cse.mrt.ac.lk>
2023-07-28 17:09:38 +1000

    Changed logger format

--- utils/logger.py
+++ utils/logger.py
@@ -3,7 +3,7 @@ import sys
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+formatter = logging.Formatter('%(message)s')
 
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
