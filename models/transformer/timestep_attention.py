b7e12729d25a17effbf11f1caf507cf276934b5b
DimuthuLakmal <kjtdimuthu.13@cse.mrt.ac.lk>
2023-10-22 14:07:41 +1100

    Added timestep attention

--- models/transformer/timestep_attention.py
+++ models/transformer/timestep_attention.py
@@ -6,13 +6,18 @@ from torch.nn import MultiheadAttention
 class TimeStepAttention(nn.Module):
     def __init__(self, emb_dim):
         super(TimeStepAttention, self).__init__()
-        self.lin_enc = nn.Linear(emb_dim, emb_dim)
+        self.lin_cross_1 = nn.Linear(emb_dim, emb_dim)
+        self.lin_cross_2 = nn.Linear(emb_dim, emb_dim)
+        self.lin_cross = nn.Linear(emb_dim * 2, emb_dim)
 
-    def forward(self, enc_x):
-        enc_x_1 = self.lin_enc(enc_x[0]).exp()
-        enc_x_2 = self.lin_enc(enc_x[1]).exp()
+    def forward(self, cross_x):
+        cross_x_1 = self.lin_cross_1(cross_x[0]).exp()
+        cross_x_2 = self.lin_cross_2(cross_x[1]).exp()
 
-        enc_x_1 = enc_x_1 / (enc_x_1 + enc_x_2 + 1e-16)
-        enc_x_2 = enc_x_2 / (enc_x_1 + enc_x_2 + 1e-16)
+        cross_x_1 = cross_x_1 / (cross_x_1 + cross_x_2 + 1e-16)
+        cross_x_2 = cross_x_2 / (cross_x_1 + cross_x_2 + 1e-16)
 
-        return [enc_x_1, enc_x_2]
+        cross_x = torch.concat((cross_x_1, cross_x_2), dim=-1)
+        cross_x = self.lin_cross(cross_x)
+
+        return cross_x
