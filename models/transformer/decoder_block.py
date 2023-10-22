Fixed an issue in softmax

models/transformer/decoder_block.py
@@ -33,7 +33,7 @@ class DecoderBlock(nn.Module):
         self.norm3 = nn.LayerNorm(emb_dim)
         self.dropout2 = nn.Dropout(ff_dropout)
 
-    def forward(self, x, enc_x, tgt_mask):
+    def forward(self, x, enc_x, tgt_mask, device):
         # self attention
         attention = self.self_attention(x, x, x, attn_mask=tgt_mask)  # 32x10x512
         x = self.norm1(x + self.dropout1(attention[0]))
@@ -46,7 +46,7 @@ class DecoderBlock(nn.Module):
             else:
                 cross_attn.append(layer(x, enc_x[idx], enc_x[idx]))
 
-        cross_attn = self.timestep_attention(cross_attn)
+        cross_attn = self.timestep_attention(cross_attn, device)
         # enc_x = torch.concat(enc_x, dim=1)
         # cross_attn = self.cross_attn_layers[0](x, enc_x, enc_x)
 

models/transformer/timestep_attention.py
@@ -1,6 +1,6 @@
 from torch import nn
+# import torch.nn.functional as F
 import torch
-from torch.nn import MultiheadAttention
 
 
 class TimeStepAttention(nn.Module):
@@ -9,15 +9,19 @@ class TimeStepAttention(nn.Module):
         self.lin_cross_1 = nn.Linear(emb_dim, emb_dim)
         self.lin_cross_2 = nn.Linear(emb_dim, emb_dim)
         self.lin_cross = nn.Linear(emb_dim * 2, emb_dim)
+        self.softmax = torch.nn.Softmax(dim=2)
 
-    def forward(self, cross_x):
-        cross_x_1 = self.lin_cross_1(cross_x[0]).exp()
-        cross_x_2 = self.lin_cross_2(cross_x[1]).exp()
+    def forward(self, cross_x, device):
+        cross_x_exp = torch.zeros((cross_x[0].shape[0], cross_x[0].shape[1], 2, cross_x[0].shape[2])).to(device)
+        cross_x_1 = self.lin_cross_1(cross_x[0])
+        cross_x_2 = self.lin_cross_2(cross_x[1])
 
-        cross_x_1 = cross_x_1 / (cross_x_1 + cross_x_2 + 1e-16)
-        cross_x_2 = cross_x_2 / (cross_x_1 + cross_x_2 + 1e-16)
+        cross_x_exp[:, :, 0] = cross_x_1
+        cross_x_exp[:, :, 1] = cross_x_2
 
-        cross_x = torch.concat((cross_x_1, cross_x_2), dim=-1)
-        cross_x = self.lin_cross(cross_x)
+        cross_x_softmax = self.softmax(cross_x_exp)
+        cross_x_softmax_reshaped = cross_x_softmax.view(cross_x_softmax.shape[0], cross_x_softmax.shape[1], -1)
+
+        cross_x = self.lin_cross(cross_x_softmax_reshaped)
 
         return cross_x

models/transformer/transformer_decoder.py
@@ -128,6 +128,6 @@ class TransformerDecoder(nn.Module):
                     enc_xs.append(
                         f_layer(enc_x[0][:, start: start + self.per_enc_feature_len].transpose(2, 1)).transpose(2, 1))
 
-            out_d = layer(out_d, enc_xs, tgt_mask)
+            out_d = layer(out_d, enc_xs, tgt_mask, device)
 
         return self._return_mat(out_d, embed_shp)