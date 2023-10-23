Fixed an issue in softmax

models/transformer/decoder_block.py
@@ -39,16 +39,16 @@ class DecoderBlock(nn.Module):
         x = self.norm1(x + self.dropout1(attention[0]))
 
         # cross attention
-        cross_attn = []
-        for idx, layer in enumerate(self.cross_attn_layers):
-            if idx == 0:
-                cross_attn.append(layer(x, enc_x[idx], enc_x[idx]))
-            else:
-                cross_attn.append(layer(x, enc_x[idx], enc_x[idx]))
+        enc_x = self.timestep_attention(enc_x, device)
+        # cross_attn = []
+        # for idx, layer in enumerate(self.cross_attn_layers):
+        #     if idx == 0:
+        #         cross_attn.append(layer(x, enc_x[idx], enc_x[idx]))
+        #     else:
+        #         cross_attn.append(layer(x, enc_x[idx], enc_x[idx]))
 
-        cross_attn = self.timestep_attention(cross_attn, device)
-        # enc_x = torch.concat(enc_x, dim=1)
-        # cross_attn = self.cross_attn_layers[0](x, enc_x, enc_x)
+        enc_x = torch.concat(enc_x, dim=1)
+        cross_attn = self.cross_attn_layers[0](x, enc_x, enc_x)
 
         x = self.norm2(x + cross_attn)
 

models/transformer/timestep_attention.py
@@ -1,27 +1,23 @@
 from torch import nn
-# import torch.nn.functional as F
+import torch.nn.functional as F
 import torch
 
 
 class TimeStepAttention(nn.Module):
     def __init__(self, emb_dim):
         super(TimeStepAttention, self).__init__()
-        self.lin_cross_1 = nn.Linear(emb_dim, emb_dim)
-        self.lin_cross_2 = nn.Linear(emb_dim, emb_dim)
-        self.lin_cross = nn.Linear(emb_dim * 2, emb_dim)
-        self.softmax = torch.nn.Softmax(dim=2)
+        self.lin_enc_1 = nn.Linear(emb_dim, emb_dim)
+        self.lin_enc_2 = nn.Linear(emb_dim, emb_dim)
+        self.lin_enc = nn.Linear(emb_dim * 2, emb_dim)
 
-    def forward(self, cross_x, device):
-        cross_x_exp = torch.zeros((cross_x[0].shape[0], cross_x[0].shape[1], 2, cross_x[0].shape[2])).to(device)
-        cross_x_1 = self.lin_cross_1(cross_x[0])
-        cross_x_2 = self.lin_cross_2(cross_x[1])
+    def forward(self, enc_x, device):
+        enc_x_exp = torch.zeros((enc_x[0].shape[0], enc_x[0].shape[1], 2, enc_x[0].shape[2])).to(device)
+        enc_x_1 = self.lin_enc_1(enc_x[0])
+        enc_x_2 = self.lin_enc_2(enc_x[1])
 
-        cross_x_exp[:, :, 0] = cross_x_1
-        cross_x_exp[:, :, 1] = cross_x_2
+        enc_x_exp[:, :, 0] = enc_x_1
+        enc_x_exp[:, :, 1] = enc_x_2
 
-        cross_x_softmax = self.softmax(cross_x_exp)
-        cross_x_softmax_reshaped = cross_x_softmax.view(cross_x_softmax.shape[0], cross_x_softmax.shape[1], -1)
+        enc_x_softmax = F.softmax(enc_x_exp, dim=2)
 
-        cross_x = self.lin_cross(cross_x_softmax_reshaped)
-
-        return cross_x
+        return [enc_x_softmax[:, :, 0] * enc_x_1, enc_x_softmax[:, :, 1] * enc_x_2]