9778f1ef1f883c9fc2e413ffebe1d2f24952f478
DimuthuLakmal <kjtdimuthu.13@cse.mrt.ac.lk>
2024-02-09 18:07:39 +1100

    Changes to GAT

--- models/sgat/gat.py
+++ models/sgat/gat.py
@@ -26,21 +26,29 @@ class GAT(nn.Module):
         for l in range(self.n_layers):
             in_f_size = self.out_f_sizes[l - 1] * n_heads[l - 1] if l else first_in_f_size
             concat = True if l < (self.n_layers - 1) else False
-            gat_layer = GATLayer(in_f_size, self.out_f_sizes[l], n_heads=n_heads[l], dropout=self.dropout_g, alpha=alpha,
+            gat_layer = GATLayer(in_f_size, in_f_size, n_heads=n_heads[l], dropout=self.dropout_g, alpha=alpha,
                                  concat=concat, edge_dim=edge_dim, seq_len=seq_len)
             self.layer_stack.append(gat_layer)
+
+        self.lin_out = nn.Linear(int(in_f_size/self.seq_len), self.out_f_sizes[-1])
 
     def forward(self, batch_data):
         out = ()
         for data in batch_data:
             x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+            x_shp = x[0].size()
 
             for l, gat_layer in enumerate(self.layer_stack):
-                x = F.dropout(x, p=self.dropout, training=self.training)
+                x1 = F.dropout(x[0], p=self.dropout, training=self.training)
+                x2 = F.dropout(x[1], p=self.dropout, training=self.training)
+                x = [x1, x2]
                 x = gat_layer(x, edge_attr=edge_attr, edge_index=edge_index)
+                x = x.reshape(x_shp[0], self.seq_len, -1)  # 307, 36, 288
+                x = x.permute(1, 0, 2)  # 36, 307, 288
+
                 if l < (self.n_layers - 1):
                     x = F.elu(x)
 
             out = (*out, x)
 
-        return torch.stack(out)
+        return self.lin_out(torch.stack(out))
